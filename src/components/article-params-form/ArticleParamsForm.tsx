30bdfccafb143a5d560a388aa35684e9f0f10550
Eduard <dumpweed10@gmail.com>
2024-09-10 19:55:59 +0300

    feat: added change styles

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -8,6 +8,7 @@ import {
 	OptionType,
 	backgroundColors,
 	contentWidthArr,
+	defaultArticleState,
 	fontColors,
 	fontFamilyOptions,
 	fontSizeOptions,
@@ -17,59 +18,64 @@ import { Separator } from '../separator';
 import { Text } from '../text';
 
 type TArticleParamsForm = {
-	fontSize: OptionType;
-	fontFamily: OptionType;
+	fontSizeOption: OptionType;
+	fontFamilyOption: OptionType;
 	fontColor: OptionType;
-	bgColor: OptionType;
+	backgroundColor: OptionType;
 	contentWidth: OptionType;
+	onSave?: (articleParams: TArticleParamsForm) => void;
+	onReset?: (articleParams: TArticleParamsForm) => void;
 };
 
 export const ArticleParamsForm = (props: TArticleParamsForm) => {
-	const [isOpen, setOpen] = useState(true); // Не забыть поставить false
+	const [isOpen, setOpen] = useState(false);
+
+	const { onSave, onReset } = props;
 
 	const [articleParams, setOption] = useState<TArticleParamsForm>({
-		fontFamily: props.fontFamily,
-		fontSize: props.fontSize,
+		fontFamilyOption: props.fontFamilyOption,
+		fontSizeOption: props.fontSizeOption,
 		fontColor: props.fontColor,
-		bgColor: props.bgColor,
+		backgroundColor: props.backgroundColor,
 		contentWidth: props.contentWidth,
 	});
 
 	const containerRef = useRef<HTMLElement | null>(null);
+	const formRef = useRef<HTMLFormElement | null>(null);
 
-	const changeFontSize = (option: OptionType) => {
-		setOption({
-			...articleParams,
-			fontSize: option,
-		});
-	};
-
-	const changeFontFamily = (option: OptionType) => {
-		setOption({
-			...articleParams,
-			fontFamily: option,
-		});
-	};
-
-	const changeColorFonts = (option: OptionType) => {
-		setOption({
-			...articleParams,
-			fontColor: option,
-		});
-	};
-
-	const changeBgColor = (option: OptionType) => {
-		setOption({
-			...articleParams,
-			bgColor: option,
-		});
-	};
-
-	const changeWidth = (option: OptionType) => {
-		setOption({
-			...articleParams,
-			contentWidth: option,
-		});
+	const changeOption = (option: OptionType) => {
+		switch (option.optionType) {
+			case 'fontFamily':
+				setOption({
+					...articleParams,
+					fontFamilyOption: option,
+				});
+				break;
+			case 'fontColor':
+				setOption({
+					...articleParams,
+					fontColor: option,
+				});
+				break;
+			case 'bgColor':
+				setOption({
+					...articleParams,
+					backgroundColor: option,
+				});
+				break;
+			case 'width':
+				setOption({
+					...articleParams,
+					contentWidth: option,
+				});
+				break;
+			case 'fontSize':
+				setOption({
+					...articleParams,
+					fontSizeOption: option,
+				});
+				break;
+		}
 	};
 
 	const handleClose = (evt: Event) => {
@@ -77,7 +83,7 @@ export const ArticleParamsForm = (props: TArticleParamsForm) => {
 		(evt.target as HTMLElement).dataset.target === 'article-openButton' ||
 		(evt.target as HTMLElement).dataset.select === 'select-item'
 			? null
-			: 'test'; //setOpen(false); нужно потом расскоментировать
+			: setOpen(false);
 	};
 
 	const handleOpenChange = (isOpen: boolean) => {
@@ -86,6 +92,24 @@ export const ArticleParamsForm = (props: TArticleParamsForm) => {
 		isOpen
 			? document.addEventListener('click', handleClose)
 			: document.removeEventListener('click', handleClose);
+	};
+
+	const saveParams = () => {
+		document.addEventListener('submit', (evt) => {
+			evt.preventDefault();
+		});
+		onSave ? onSave(articleParams) : null;
+	};
+
+	const resetStyle = () => {
+		setOption({
+			fontColor: defaultArticleState.fontColor,
+			fontFamilyOption: defaultArticleState.fontFamilyOption,
+			fontSizeOption: defaultArticleState.fontSizeOption,
+			backgroundColor: defaultArticleState.backgroundColor,
+			contentWidth: defaultArticleState.contentWidth,
+		});
+		onReset ? onReset(defaultArticleState) : null;
 	};
 
 	return (
@@ -98,45 +122,45 @@ export const ArticleParamsForm = (props: TArticleParamsForm) => {
 			<aside
 				className={`${styles.container} ${isOpen ? styles.container_open : ''}`}
 				ref={containerRef}>
-				<form className={styles.form}>
-					<Text as={'span'} size={31} weight={800} dynamicLite uppercase>
+				<form className={styles.form} ref={formRef}>
+					<Text as={'span'} size={31} weight={800} uppercase>
 						Задайте параметры
 					</Text>
 					<Select
-						selected={articleParams.fontFamily}
+						selected={articleParams.fontFamilyOption}
 						options={fontFamilyOptions}
 						title='Шрифт'
-						onChange={changeFontFamily}
+						onChange={changeOption}
 					/>
 					<RadioGroup
 						name='font-size-group'
 						options={fontSizeOptions}
-						selected={articleParams.fontSize}
+						selected={articleParams.fontSizeOption}
 						title='Размер шрифта'
-						onChange={changeFontSize}
+						onChange={changeOption}
 					/>
 					<Select
 						selected={articleParams.fontColor}
 						options={fontColors}
 						title='Цвет шрифта'
-						onChange={changeColorFonts}
+						onChange={changeOption}
 					/>
 					<Separator />
 					<Select
-						selected={articleParams.bgColor}
+						selected={articleParams.backgroundColor}
 						options={backgroundColors}
 						title='Цвет фона'
-						onChange={changeBgColor}
+						onChange={changeOption}
 					/>
 					<Select
 						selected={articleParams.contentWidth}
 						options={contentWidthArr}
 						title='Ширина контента'
-						onChange={changeWidth}
+						onChange={changeOption}
 					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
-						<Button title='Применить' type='submit' />
+						<Button title='Сбросить' type='reset' onClick={resetStyle} />
+						<Button title='Применить' type='submit' onClick={saveParams} />
 					</div>
 				</form>
 			</aside>
