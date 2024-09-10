b10f49a9ca4a262d72e3b2217b18aa5a00c6004e
Eduard <dumpweed10@gmail.com>
2024-09-10 10:07:39 +0300

    feat: added article form

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,65 @@
 import arrow from 'src/images/arrow.svg';
 
 import styles from './ArrowButton.module.scss';
+import { useEffect, useRef, useState } from 'react';
 
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+type TArrowButtonProp = {
+	isOpen: boolean;
+	onOpen: (isOpen: boolean) => void;
+	dataset?: string;
+};
+
+export const ArrowButton = (props: TArrowButtonProp) => {
+	let { isOpen } = props;
+	const { onOpen } = props;
+	const { dataset } = props;
+
+	const [isActive, setActive] = useState(isOpen);
+
+	const containerRef = useRef<HTMLDivElement | null>(null);
+	const arrowRef = useRef<HTMLImageElement | null>(null);
+
+	useEffect(() => {
+		isActive
+			? (containerRef.current?.classList.add(styles.container_open),
+			  arrowRef.current?.classList.add(styles.arrow_open))
+			: (containerRef.current?.classList.remove(styles.container_open),
+			  arrowRef.current?.classList.remove(styles.arrow_open));
+	}, [isActive]);
+
+	const onClickHandler: OnClick = () => {
+		isActive
+			? (setActive(false), (isOpen = false))
+			: (setActive(true), (isOpen = true));
+
+		onOpen(isOpen);
+	};
+
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
+			ref={containerRef}
+			onClick={onClickHandler}
+			data-target={dataset}
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={`
+				${styles.container}
+				${isOpen ? styles.container_open : ''}
+			`}>
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={`
+					${styles.arrow}
+					${isOpen ? styles.arrow_open : ''}
+				`}
+				ref={arrowRef}
+			/>
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,13 +2,85 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useEffect, useRef, useState } from 'react';
+import { Select } from '../select';
+import {
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
+import { RadioGroup } from '../radio-group';
+import { Separator } from '../separator';
+import { Text } from '../text';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setOpen] = useState(false);
+
+	const containerRef = useRef<HTMLElement | null>(null);
+
+	useEffect(() => {
+		isOpen
+			? containerRef.current?.classList.add(styles.container_open)
+			: containerRef.current?.classList.remove(styles.container_open);
+	}, [isOpen]);
+
+	const handleClose = (evt: Event) => {
+		containerRef.current?.contains(evt.target as HTMLElement) ||
+		(evt.target as HTMLElement).dataset.target === 'article-params'
+			? null
+			: setOpen(false);
+	};
+
+	const handleOpenChange = (isOpen: boolean) => {
+		setOpen(isOpen);
+
+		isOpen
+			? document.addEventListener('click', handleClose)
+			: document.removeEventListener('click', handleClose);
+	};
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton
+				isOpen={isOpen}
+				onOpen={handleOpenChange}
+				dataset='article-params'
+			/>
+			<aside className={styles.container} ref={containerRef}>
 				<form className={styles.form}>
+					<Text as={'span'} size={31} weight={800} dynamicLite uppercase>
+						Задайте параметры
+					</Text>
+					<Select
+						selected={defaultArticleState.fontFamilyOption}
+						options={fontFamilyOptions}
+						title='Шрифт'
+					/>
+					<RadioGroup
+						name='Шрифты'
+						options={fontSizeOptions}
+						selected={defaultArticleState.fontSizeOption}
+						title='Размер шрифта'
+					/>
+					<Select
+						selected={defaultArticleState.fontColor}
+						options={fontColors}
+						title='Цвет шрифта'
+					/>
+					<Separator />
+					<Select
+						selected={defaultArticleState.backgroundColor}
+						options={backgroundColors}
+						title='Цвет фона'
+					/>
+					<Select
+						selected={defaultArticleState.contentWidth}
+						options={contentWidthArr}
+						title='Ширина контента'
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
